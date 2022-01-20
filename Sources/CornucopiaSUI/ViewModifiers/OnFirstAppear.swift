0dbdad590d539359e14cddcbfb84ce0d30215569
Dr. Michael Lauer <mickey@vanille-media.de>
2022-01-20 12:41:25 +0100

    docs++

--- Sources/CornucopiaSUI/ViewModifiers/OnFirstAppear.swift
+++ Sources/CornucopiaSUI/ViewModifiers/OnFirstAppear.swift
@@ -3,18 +3,19 @@
 //
 import SwiftUI
 
+/// Schedules a closure to execute when the view appears, but only once.
 public struct OnFirstAppear: ViewModifier {
 
     let perform: () -> Void
 
-    @State private var firstTime: Bool = true
+    @State private var isExecutingForTheFirstTime: Bool = true
 
     public func body(content: Content) -> some View {
 
         content
             .onAppear {
-                guard self.firstTime else { return }
-                self.firstTime = false
+                guard self.isExecutingForTheFirstTime else { return }
+                self.isExecutingForTheFirstTime = false
                 self.perform()
             }
     }
@@ -22,6 +23,7 @@ public struct OnFirstAppear: ViewModifier {
 
 extension View {
 
+    /// Executes the closure when the view first appears.
     public func CC_onFirstAppear(perform: @escaping () -> Void ) -> some View {
         self.modifier(OnFirstAppear(perform: perform))
     }
