c742364b9fe7d37667b83fef918220d93fc63425
Dr. Michael Lauer <mickey@vanille-media.de>
2022-04-08 17:43:23 +0200

    Views: Add BusyButton.

---
+++ Sources/CornucopiaSUI/Views/BusyButton.swift
@@ -0,0 +1,69 @@
+//
+//  Cornucopia – (C) Dr. Lauer Information Technology
+//
+import SwiftUI
+
+/// A basic button that shows a 'busy' behavior when triggered.
+public struct BusyButton: View {
+
+    public typealias ActionFunc = () async throws -> ()
+
+    @Binding var isBusy: Bool
+    @State private var title: String
+    @State private var action: ActionFunc
+
+    public var body: some View {
+
+        ZStack {
+            Button(action: {
+                self.isBusy = true
+                Task {
+                    defer { DispatchQueue.main.async { self.isBusy = false} }
+                    try await self.action()
+                }
+            }) {
+                Text(title)
+                    .opacity(isBusy ? 0 : 1)
+            }
+            .disabled(isBusy)
+
+            ProgressView()
+                .opacity(isBusy ? 1 : 0)
+                .saturation(-1)
+        }
+        .animation(.easeInOut, value: isBusy)
+    }
+
+    public init(isBusy: Binding<Bool>, title: String, action: @escaping ActionFunc) {
+        self._isBusy = isBusy
+        self.title = title
+        self.action = action
+    }
+}
+
+//MARK: - Example
+private struct BusyButtonExample: View {
+
+    @State private var isBusy: Bool = false
+
+    var body: some View {
+
+        BusyButton(isBusy: $isBusy, title: "Click Me!") {
+            print("I have been clicked!")
+            try await Task.sleep(nanoseconds: NSEC_PER_SEC * 2)
+            print("done")
+        }
+        .tint(.green)
+        .buttonStyle(.bordered)
+        .controlSize(.large)
+    }
+}
+
+struct BusyButton_Previews: PreviewProvider {
+
+    @State private var isBusy: Bool = true
+
+    static var previews: some View {
+        BusyButtonExample()
+    }
+}
