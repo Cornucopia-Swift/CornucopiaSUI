9f8625e190a2309013100d5f4439ea7630892c4d
Dr. Michael Lauer <mickey@vanille-media.de>
2022-09-04 14:57:05 +0200

    Color+ForeignFrameworkColors: add separatorColor and opaqueSeparatorColor from UIKit.

--- Sources/CornucopiaSUI/Extensions/Color+ForeignFrameworkColors.swift
+++ Sources/CornucopiaSUI/Extensions/Color+ForeignFrameworkColors.swift
@@ -3,43 +3,29 @@
 //
 import SwiftUI
 
+#if os(iOS) || os(tvOS) || os(watchOS)
+import UIKit
+
+public extension Color {
+    static var primaryBackground: Color { Color(.systemBackground) }
+    static var secondaryBackground: Color { Color(.secondarySystemBackground) }
+    static var tertiaryBackground: Color { Color(.tertiarySystemBackground) }
+
+    static var primaryGroupedBackground: Color { Color(.systemGroupedBackground) }
+    static var secondaryGroupedBackground: Color { Color(.secondarySystemGroupedBackground) }
+    static var tertiaryGroupedBackground: Color { Color(.tertiarySystemGroupedBackground) }
+
+    static var tertiary: Color { Color(.tertiaryLabel) }
+    static var separatorColor: Color { Color(.separator) }
+    static var opaqueSeparatorColor: Color { Color(.opaqueSeparator) }
+}
+#elseif os(macOS)
+import AppKit
+
 public extension Color {
 
-#if os(iOS) || os(tvOS) || os(watchOS)
+    static var primaryBackground: Color { Color(.textBackgroundColor) }
+    static var tertiary: Color { Color(.tertiaryLabelColor) }
 
-    static var primaryBackground: Color {
-        Color(.systemBackground)
-    }
-    static var secondaryBackground: Color {
-        Color(.secondarySystemBackground)
-    }
-    static var tertiaryBackground: Color {
-        Color(.tertiarySystemBackground)
-    }
-
-    static var primaryGroupedBackground: Color {
-        Color(.systemGroupedBackground)
-    }
-    static var secondaryGroupedBackground: Color {
-        Color(.secondarySystemGroupedBackground)
-    }
-    static var tertiaryGroupedBackground: Color {
-        Color(.tertiarySystemGroupedBackground)
-    }
-
-    static var tertiary: Color {
-        Color(.tertiaryLabel)
-    }
-
-#elseif os(macOS)
-
-    static var primaryBackground: Color {
-        Color(.textBackgroundColor)
-    }
-
-    static var tertiary: Color {
-        Color(.tertiaryLabelColor)
-    }
-#endif
 }
-
+#endif
