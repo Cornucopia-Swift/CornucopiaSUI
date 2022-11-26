93ad00219297d8327f3148591ee5844bac5c8cc5
Dr. Michael Lauer <mickey@vanille-media.de>
2022-11-26 11:55:28 +0100

    ViewModifiers: Add DebugPrinting

---
+++ Sources/CornucopiaSUI/ViewModifiers/DebugPrinting.swift
@@ -0,0 +1,14 @@
+//
+//  Cornucopia – (C) Dr. Lauer Information Technology
+//
+import SwiftUI
+
+public extension View {
+
+    func CC_debugPrinting(_ value: Any) -> some View {
+#if DEBUG
+        print(value)
+        return self
+#endif
+    }
+}
