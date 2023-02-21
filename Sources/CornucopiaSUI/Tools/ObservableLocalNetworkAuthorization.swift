5a42f6bebfdd828251b745df9be72aced0de12c6
Dr. Michael Lauer <mickey@vanille-media.de>
2023-02-21 17:42:34 +0100

    Tools: ObservableLocalNetworkAuthorization: Docs++
    This documents a potential cause for an unintended UI because
    of the delayed transition from `.notDetermined` to `.granted`.

--- Sources/CornucopiaSUI/Tools/ObservableLocalNetworkAuthorization.swift
+++ Sources/CornucopiaSUI/Tools/ObservableLocalNetworkAuthorization.swift
@@ -8,7 +8,19 @@ import OSLog
 
 /// Observes the local network authorization.
 /// Inspired by discussions and solutions at https://stackoverflow.com/questions/63940427/ios-14-how-to-trigger-local-network-dialog-and-check-user-answer
-/// To save resources, this class stops further work as soon as the status has moved to `.granted`.
+/// To save resources, this class stops further work once the status has transitioned to `.granted`.
+///
+/// NOTE: If you're using this class to show a warning UI while the authorization dialog is initially visible,
+/// you might have the problem that ­– since the status always starts out with `.notDetermined` ­–
+/// your UI might shortly display the wrong state, since the transition `.notDetermined` -> `.granted` happens
+/// only after a "short while". If you don't want that, it's advised to delay the evaluation for
+/// about 1 second. If it takes longer than 1 second to publish the network service locally,
+/// we can safely assume that it didn't work due to a permission problem.
+
+/// Thinking about it, we could perhaps code this logic into this class and start out with an `.unknown` state first,
+/// before transitioning afterwards into one of the three other states. Then again, I usually don't like
+/// to hard code timings in such a way, so it might be better to fix this in your UI layer. Please give feedback,
+/// if you have an opinion on that.
 public class ObservableLocalNetworkAuthorization: NSObject, ObservableObject, NetServiceDelegate {
 
     public enum Status {
@@ -66,6 +78,10 @@ public class ObservableLocalNetworkAuthorization: NSObject, ObservableObject, Ne
     public func netServiceDidPublish(_ sender: NetService) {
         os_log("NetService did publish", log: OSLog.default, type: .debug)
         self.status = .granted
+    }
+
+    public func netService(_ sender: NetService, didNotPublish errorDict: [String : NSNumber]) {
+        os_log("NetService did not publish: %s", log: OSLog.default, type: .debug, errorDict.description)
     }
 
     private func shutdown() {
