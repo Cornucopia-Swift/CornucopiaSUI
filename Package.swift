a7fce24f7d4d54b0150fe1fb828a017c7c5c91dc
Dr. Michael Lauer <mickey@vanille-media.de>
2025-02-25 16:31:05 +0100

    bump requirements because of the use of NavigationPath

--- Package.swift
+++ Package.swift
@@ -5,10 +5,10 @@ import PackageDescription
 let package = Package(
     name: "CornucopiaSUI",
     platforms: [
-        .macOS(.v12),
-        .iOS(.v15),
-        .tvOS(.v15),
-        .watchOS(.v8)
+        .macOS(.v13),
+        .iOS(.v16),
+        .tvOS(.v16),
+        .watchOS(.v9)
     ],
     products: [
         .library(
