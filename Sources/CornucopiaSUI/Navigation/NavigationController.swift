c12d9fa0191d6b7d50a2bebf9316c094dda97302
Dr. Michael Lauer <mickey@vanille-media.de>
2025-02-27 15:47:53 +0100

    NavigationController: Implement pathContains using type identity.

--- Sources/CornucopiaSUI/Navigation/NavigationController.swift
+++ Sources/CornucopiaSUI/Navigation/NavigationController.swift
@@ -10,30 +10,44 @@ private let logger = Cornucopia.Core.Logger()
 public final class NavigationController: ObservableObject {
 
     @Published public var path = NavigationPath()
+    private var types: [String] = []
 
     public init() {}
 
+    /// Pushes a new element onto the navigation stack.
     public func push<T: Hashable>(_ element: T) {
         logger.debug("Pushing \(element) on the navigation stack")
+        self.types.append(String(describing: T.self))
         withAnimation {
             self.path.append(element)
         }
     }
 
+    /// Pops a number of elements from the navigation stack.
     public func pop(_ count: Int = 1) {
         guard count > 0 else { return }
         logger.debug("Popping \(count) elements from the navigation stack")
         let numberToRemove = min(count, self.path.count)
+        self.types.removeLast(numberToRemove)
         withAnimation {
             self.path.removeLast(numberToRemove)
         }
     }
 
+    /// Pops all elements from the navigation stack.
     public func popToRoot() {
         logger.debug("Popping to root of the navigation stack")
+        self.types.removeAll()
         withAnimation {
             self.path.removeLast(self.path.count)
         }
+    }
+
+    /// Returns whether the navigation stack contains a certain type of element.
+    /// NOTE: This is not using equality, but type identity!
+    public func pathContains<T: Hashable>(_ element: T) -> Bool {
+        let typename = String(describing: T.self)
+        return self.types.contains(typename)
     }
 }
 
